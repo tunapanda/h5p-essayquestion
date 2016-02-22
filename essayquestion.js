a6ec2cf5792caf1f72c3572788b14e0debd790a8
Brad Smith <brad@geekdome.net>
2016-02-21 22:23:15 -0500

    preliminary support for an xAPISuccess event

--- essayquestion.js
+++ essayquestion.js
@@ -20,6 +20,25 @@ H5P.EssayQuestion = (function ($) {
     // Keep provided id.
     this.id = id;
     this.response = undefined;
+    
+    /* 
+    TODO: the host's xAPI handler should fire an xAPISuccess event when submission of an xAPI statement is confirmed to have happened. 
+    Example:
+    
+      document.dispatchEvent(
+      	new CustomEvent('xAPISuccess',{
+      		detail: {
+      			* some properties *
+      		}
+      	})
+      )
+      
+    Once this functionality is in place, the stuff that makes the input read-only, which is currently in response_submitted() should be moved to this listener.
+    */
+    document.addEventListener('xAPISuccess',function(evt){
+    	console.log("Listener fired!");
+    	console.log(evt);		
+    });
   };
   
   // Inherit from H5P.question
@@ -37,10 +56,14 @@ H5P.EssayQuestion = (function ($) {
   // Make input read-only, submit via xAPI
   C.prototype.response_submitted = function() {
 	// Replace input with read-only text
+	var button = $("button.h5p-question-essayquestion-submit");
+	button.attr("disabled","disabled");
+	button.html("Answer Submitted");
+	
 	var input = $(".essayquestion-input");
 	this.response = input.val();
 	var ro = $("<div>");
-	ro.addClass(".essayquestion-input-ro");
+	ro.addClass("essayquestion-input-ro");
 	ro.html(this.response);
 	input.replaceWith(ro);
 	
